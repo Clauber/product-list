3ffff3061b060b47082b04a54cb7e096a4fb7621
Clauber Oliveira <cklauber@gmail.com>
2020-09-24 09:37:32 -0600

    Updates

---
+++ precache-manifest.ceebb7714c4110e242c546779e33b7a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2832d787c4b8ae3838de00a42e1688dc",
+    "url": "/product-list/index.html"
+  },
+  {
+    "revision": "9eadada5fe2e32f427ec",
+    "url": "/product-list/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4388e7c880fac1043649",
+    "url": "/product-list/static/css/main.995985ba.chunk.css"
+  },
+  {
+    "revision": "9eadada5fe2e32f427ec",
+    "url": "/product-list/static/js/2.8f397917.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/product-list/static/js/2.8f397917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4388e7c880fac1043649",
+    "url": "/product-list/static/js/main.301b1b08.chunk.js"
+  },
+  {
+    "revision": "34bd088c12b7ef590995",
+    "url": "/product-list/static/js/runtime-main.10a88bcc.js"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/product-list/static/media/react-logo.70f87bba.png"
+  }
+]);
